ef26e7a1919ebb09e82b3e81a987d8824bfaf574
Peter John Lee <47859286+pjl3dm@users.noreply.github.com>
2025-03-28 18:02:16 +0800

    fix: simplify suitecloud.config.js by removing obsolete commands and updating authentication method

--- suitecloud.config.js
+++ suitecloud.config.js
@@ -1,22 +1,13 @@
-const SuiteCloudJestUnitTestRunner = require('@oracle/suitecloud-unit-testing/services/SuiteCloudJestUnitTestRunner');
-
 module.exports = {
-    defaultProjectFolder: 'src',
-    commands: {
-        "project:deploy": {
-            beforeExecuting: () => {
-                return true;
-            },
-        },
-        'project:validate': {
-            beforeExecuting: async args => {
-                args.accountspecificvalues = 'WARNING';
-                args.server = true;
-                return args;
-            }
-        }
-    },
-    accountSettings: {
-        authenticationMode: 'token'
-    }
+  defaultProjectFolder: 'src',
+  commands: {
+      "project:deploy": {
+          beforeExecuting: () => {
+              return true;
+          },
+      },
+  },
+  accountSettings: {
+      authenticationMethod: 'token'
+  }
 };
