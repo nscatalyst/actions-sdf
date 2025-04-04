6bb267caa2082105e6d3740a41772f2b5a5af0f2
Peter John Lee <47859286+pjl3dm@users.noreply.github.com>
2025-04-04 22:44:21 +0800

    feat: implement Suitelet form for user input and submission handling

--- src/FileCabinet/SuiteScripts/ctc_test_script_vcs1.js
+++ src/FileCabinet/SuiteScripts/ctc_test_script_vcs1.js
@@ -1,2 +1,42 @@
-//Test script from VCS
-//test if this actually worked
+/* CLICKUP-TASK-ID: TASK1 */
+/* NS-DEPLOY-TARGET: 354290 */
+/**
+ * @NApiVersion 1.x
+ * @NScriptType Suitelet
+ */
+function onRequest(context) {
+    // Check if the request is GET or POST
+    if (context.request.method === 'GET') {
+        // Create a form
+        var form = nlapiCreateForm('Sample Suitelet Form');
+
+        // Add fields to the form
+        var nameField = form.addField('custpage_name', 'text', 'Name');
+        var emailField = form.addField('custpage_email', 'text', 'Email');
+
+        // Add a submit button
+        form.addSubmitButton('Submit');
+
+        // Render the form to the screen
+        context.response.writePage(form);
+
+    } else { // POST request
+        // Get the submitted values
+        var name = context.request.getParameter('custpage_name');
+        var email = context.request.getParameter('custpage_email');
+
+        // Create a response message
+        var form = nlapiCreateForm('Submission Result');
+        form.addField('custpage_message', 'inlinehtml', 'Result');
+        var message = 'You submitted the following information:<br>';
+        message += 'Name: ' + name + '<br>';
+        message += 'Email: ' + email;
+        form.getField('custpage_message').setDefaultValue(message);
+
+        // Add a button to go back to the original form
+        form.addButton('custpage_back_button', 'Go Back', 'window.history.back()');
+
+        // Render the form with the result message
+        context.response.writePage(form);
+    }
+}
